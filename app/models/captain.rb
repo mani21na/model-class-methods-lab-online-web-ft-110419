c42dc633e1e97c2085de4bea20ae38852f471b57
Hanna Lee <mani21na@gmail.com>
2020-02-10 22:41:17 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,5 +1,6 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
 
   def self.catamaran_operators
     # includes(boats: :classifications).where(classifications: {name: "Catamaran"})
