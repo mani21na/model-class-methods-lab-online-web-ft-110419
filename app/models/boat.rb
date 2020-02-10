b9945454a17584291a9a53b56478a38382eb9336
Hanna Lee <mani21na@gmail.com>
2020-02-10 22:40:26 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -36,8 +36,7 @@ class Boat < ActiveRecord::Base
   end
 
   def self.non_sailboats
-    # where("id NOT IN (?)", self.sailboats.pluck(:id))
-    includes('classifications').where(classifications: {name: 'Sailboat'})
+    where("id NOT IN (?)", self.sailboats.pluck(:id))
   end
 
   def self.longest
